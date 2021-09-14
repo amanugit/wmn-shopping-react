ae9e51f991f457789b300affdd8345941cd1535a
Amanuel <amannew49@gmail.com>
2021-09-14 06:59:59 -0700

    this is some fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,7 +43,7 @@ function App() {
           <Route path="/admin/login" component={AdminLogin} exact></Route>
           <Route path="/admin/:key" component={Admin} exact></Route>
           <Route
-            path="/admin/:key/:skey/currentPageNo"
+            path="/admin/:key/:skey/:currentPageNo"
             component={Admin}
             exact
           ></Route>
