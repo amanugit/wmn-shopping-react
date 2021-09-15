a6af36eca30f810e1d56cf77e216a4354a58172e
Amanuel <amannew49@gmail.com>
2021-09-15 09:18:29 -0700

    this

--- frontend/src/screens/admin/Admin.js
+++ frontend/src/screens/admin/Admin.js
@@ -49,15 +49,15 @@ function Admin({ match, history }) {
   const deletePrd = (id) => {
     alert("Opps, this feature is not applied! this is for demo purpose");
   };
-
+  const searchProduct = (e) => {
+    e.preventDefault();
+    if (searchTerm === "") {
+      return;
+    }
+    history.push(`/admin/filter/products/${searchTerm}/${1}/`);
+  };
   useCallback(() => {
-    const searchProduct = (e) => {
-      e.preventDefault();
-      if (searchTerm === "") {
-        return;
-      }
-      history.push(`/admin/filter/products/${searchTerm}/${1}/`);
-    };
+    searchProduct();
   }, [searchTerm, history]);
   useEffect(() => {
     if (userInfo && userInfo.isAdmin) {
