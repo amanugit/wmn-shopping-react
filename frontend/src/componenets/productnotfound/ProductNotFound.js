3d97d7cd9760f36583356ac41e67f7301d3678e4
Amanuel <amannew49@gmail.com>
2022-06-07 13:57:53 +0300

    this

--- frontend/src/componenets/productnotfound/ProductNotFound.js
+++ frontend/src/componenets/productnotfound/ProductNotFound.js
@@ -36,7 +36,7 @@ function ProductNotFound({ supCat, categoryQ, history }) {
     };
   }, []);
   return (
-    <div className={`modalnotfound ${showModal} ? modalShown : modalHidden` }
+    <div className={`modalnotfound ${showModal} ? modalShown : modalHidden`}
     >
       <div className="notfoundContainer">
         <div>
