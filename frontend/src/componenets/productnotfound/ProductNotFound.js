b0cab9356313da0027d70416a89cab4cca3306a0
Amanuel <amannew49@gmail.com>
2022-06-07 14:47:27 +0300

    this

--- frontend/src/componenets/productnotfound/ProductNotFound.js
+++ frontend/src/componenets/productnotfound/ProductNotFound.js
@@ -1,12 +1,7 @@
 import React, { useEffect, useState } from "react";
 import {getProductsByFilter, getProductsByFilterCat, listProducts, getSubCategory } from '../../actions/productActions';
 import { useDispatch } from "react-redux";
-import { withRouter } from "react-router-dom";
-import {
-  PRODUCT_LIST_RESET,
-  PRODUCT_GET_BYFILTER_RESET,
-  PRODUCT_GET_CAT_BYFILTER_RESET,
-} from "../../constants/productConstants";
+
 import { withRouter } from "react-router-dom";
 function ProductNotFound({ supCat, categoryQ, history }) {
   const dispatch = useDispatch();
